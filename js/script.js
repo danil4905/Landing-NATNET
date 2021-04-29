55bbd154562328f5376f09c9e842cd5cb5ef73b7
Danil <conovalov.danil3441@gmail.com>
2021-04-29 19:09:07 +0400

    Animation fixed

--- js/script.js
+++ js/script.js
@@ -63,5 +63,5 @@ $("#header-menu__list").on("click","a", function (event) {
         event.preventDefault();
         var id  = $(this).attr('href'),
             top = $(id).offset().top;
-        $('body,html').animate({scrollTop: top},10);
+        $('body,html').animate({scrollTop: top},500);
     });
